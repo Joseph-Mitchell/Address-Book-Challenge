cafaf7e6c193dd455a177b12f40c8cfff237d429
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-05-18 20:58:58 +0100

    User Story 1 - Test 38: All Tests Passing

--- src/test/java/com/digitalfuturesacademy/app/ValidateTest.java
+++ src/test/java/com/digitalfuturesacademy/app/ValidateTest.java
@@ -155,19 +155,19 @@ public class ValidateTest {
             //Assert
             assertFalse(actual);
         }
-//
-//        @Test
-//        @DisplayName("Returns false if string only whitespace")
-//        void falseIfBlank() {
-//            //Arrange
-//            String testInput = " ";
-//
-//            //Act
-//            boolean actual = Validate.string(testInput);
-//
-//            //Assert
-//            assertFalse(actual);
-//        }
+
+        @Test
+        @DisplayName("Returns false if string only whitespace")
+        void falseIfBlank() {
+            //Arrange
+            String testInput = " ";
+
+            //Act
+            boolean actual = Validate.phone(testInput);
+
+            //Assert
+            assertFalse(actual);
+        }
 //
 //        @Test
 //        @DisplayName("Returns false if string null")
